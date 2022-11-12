f12fca5bc1b1567f5db91a98062d62d33c05950d
PathakamuriNaresh <114144498+PathakamuriNaresh@users.noreply.github.com>
2022-11-12 23:28:47 +0530

    Update test.sv

--- test_dir/test.sv
+++ test_dir/test.sv
@@ -1,9 +1,14 @@
 class my_comp extends uvm_component;
 	`uvm_component_utils(my_comp)
 
-	function new(input string path="my_comp", parent=null);
+	function void new(input string path="my_comp", parent=null);
 		super.new(path,parent);
 	endfunction
 
 endclass
+
+function build_phase(uvm_phase, phase);
+	super.build_phase(phase);
+endfunction
+
 
